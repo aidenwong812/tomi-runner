8b6c71a0dd13f69a4c2bc28cb9c140f32fc52ddc
Aiden Wong <wongaiden812@gmail.com>
2024-06-29 13:39:26 +0800

    fix deploy bug.
    Co-authored-by: Aiden Wong <wongaiden812@gmail.com>
    Co-authored-by: Maksym Poplavskyi <vincenzagiorgi6@gmail.com>

---
+++ src/app/_components/layout/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <div className="px-5 py-8">
+      <div className="border-b border-[#EFEFEF]"></div>
+      <div className="flex justify-between items-end px-2 pt-6 text-[#A5A5A5]">
+        <p>&copy;2023 tomi, all rights reserved</p>
+        <p>Terms of Use | Privacy Policy</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
