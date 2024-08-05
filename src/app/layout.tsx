03fcf7f8a83857564dbba2458411df8240421252
Ruslan Vinnichuk <vincenzagiorgi6@gmail.com>
2024-08-05 23:46:07 +0800

    update add fund modal.
    Co-Authored-By: lionstar <132696312+lionstar259007@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 import "@/styles/globals.css"
+import 'react-toastify/dist/ReactToastify.css';
 
 import { Inter } from "next/font/google"
 import Image from "next/image"
-import { ThirdwebProvider } from "thirdweb/react"
+import { AutoConnect, ThirdwebProvider } from "thirdweb/react"
+import { createWallet } from "thirdweb/wallets";
+import { ToastContainer } from 'react-toastify';
 
 import { TRPCReactProvider } from "@/trpc/react"
-import Header from "./_components/layout/Header"
+import { thirdwebClient } from "@/utils/thirdweb-client";
 import Background from "@/assets/images/background.png"
+import Header from "./_components/layout/Header"
 import Footer from "./_components/layout/Footer"
 
 const inter = Inter({
@@ -18,6 +22,19 @@ export const metadata = {
   title: "Tomi Runner",
   description: "Tomi Runner",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+}
+
+const wallets = [
+  createWallet("com.tomi"),
+  createWallet("io.metamask"),
+  createWallet("walletConnect"),
+]
+
+const appMetadata = {
+  name: "Tomi Runner",
+  description: "Deploy CI/CD GitHub Actions workflows to Decentralized physical infrastructure network (DePIN)",
+  url: process.env.NEXT_PUBLIC_BASE_URL,
+  logoUrl: "/favicon.ico",
 }
 
 export default function RootLayout({
@@ -34,6 +51,8 @@ export default function RootLayout({
             <Header />
             {children}
             <Footer />
+            <AutoConnect client={thirdwebClient} wallets={wallets} appMetadata={appMetadata} />
+            <ToastContainer autoClose={3000} newestOnTop theme="dark" />
           </TRPCReactProvider>
         </ThirdwebProvider>
       </body>
